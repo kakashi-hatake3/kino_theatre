70ed47586a65004fdaec35329ac29a077c399804
kakashi-hatake3 <mayorov.art7@gmail.com>
2022-10-21 19:20:58 +0300

    add include add new app subsidiary

--- kinotheatre/urls.py
+++ kinotheatre/urls.py
@@ -14,7 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from homepage.views import *
 urlpatterns = [
+    path('', include('homepage.urls')),
+    path('subs/', include('subsidiary.urls')),
     path('admin/', admin.site.urls),
 ]
