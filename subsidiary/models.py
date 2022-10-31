2f3e276289c5adef95c3ea4bd0837202a4e92c87
kakashi-hatake3 <mayorov.art7@gmail.com>
2022-11-01 00:30:26 +0300

    first model

--- subsidiary/models.py
+++ subsidiary/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Subsidiaryies(models.Model):
+    count_halls = models.IntegerField()
+    describe = models.TextField(blank=True)
+    address = models.CharField(max_length=150)
+
