dd07cd2fa91778f14db08b0cb640c0fa52b32ee8
kakashi-hatake3 <mayorov.art7@gmail.com>
2022-11-01 00:42:11 +0300

    update model make migration: python manage.py makemigrations

--- subsidiary/models.py
+++ subsidiary/models.py
@@ -5,4 +5,5 @@ class Subsidiaryies(models.Model):
     count_halls = models.IntegerField()
     describe = models.TextField(blank=True)
     address = models.CharField(max_length=150)
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
 
